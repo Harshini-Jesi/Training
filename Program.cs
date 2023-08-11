f58076d608fe554f2245d04bd4a1ebc63a97b43a
Harshini <harshini.s@metamation.com>
2023-08-11 16:28:43 +0530

    T03 completed

--- Program.cs
+++ Program.cs
@@ -1,41 +1,22 @@
 ﻿using System;
-namespace numberconversion {
+namespace Fibonacci {
     class Program {
-        static string[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        static string[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-        static string Cwords (int num) {
-            if (num < 0)
-                return ("Negative" + Cwords (-num));
-            if (num < 20)
-                return ones[num];
-            if (num < 100)
-                return tens[num / 10] + (num % 10 != 0 ? " " + ones[num % 10] : " ");
-            if (num < 1000)
-                return ones[num / 100] + " hundred" + (num % 100 != 0 ? " and " + Cwords (num % 100) : "");
-            if (num < 1000000)
-                return Cwords (num / 1000) + " thousand" + (num % 1000 != 0 ? " " + Cwords (num % 1000) : "");
-            return "Number out of range";
-        }
-        static string ConvertToRoman (int num) {
-            if (num < 1 || num > 3999) {
-                return "Out of Roman numeral range";
-            }
-            string[] romansym = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-            int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-            string romanNo = "";
-            for (int i = 0; i < values.Length; i++) {
-                while (num >= values[i]) { romanNo += romansym[i]; num -= values[i]; }
-            }
-            return romanNo;
-        }
         static void Main (string[] args) {
-            Console.Write ("Enter a number : ");
-            int num = int.Parse (Console.ReadLine ());
-            string words = Cwords (num);
-            Console.WriteLine ($"Number in words : {words}");
-            string roman = ConvertToRoman (num);
-            Console.WriteLine ($"Number in romans : {roman}");
+            Console.Write ("Input : ");
+            int n = int.Parse (Console.ReadLine ());
+            Console.Write ("Fibonacci series : ");
+            for (int i = 0; i < n; i++) { Console.Write (fib (i) + " "); }
+        }
+        static int fib (int n) {
+            if (n <= 1)
+                return n;
+            int fib1 = 0, fib2 = 1, fib3 = 0;
+            for (int i = 2; i <= n; i++) {
+                fib3 = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fib3;
+            }
+            return fib3;
         }
     }
 }
