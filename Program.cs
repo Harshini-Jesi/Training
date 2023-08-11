6a5240f8662cf165e924e3ea605a27d54c8ebef1
Harshini <harshini.s@metamation.com>
2023-08-11 16:17:29 +0530

    T02 completed

--- Program.cs
+++ Program.cs
@@ -1,37 +1,41 @@
 ﻿using System;
-namespace decimalconversion {
+namespace numberconversion {
     class Program {
+        static string[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        static string[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        static string Cwords (int num) {
+            if (num < 0)
+                return ("Negative" + Cwords (-num));
+            if (num < 20)
+                return ones[num];
+            if (num < 100)
+                return tens[num / 10] + (num % 10 != 0 ? " " + ones[num % 10] : " ");
+            if (num < 1000)
+                return ones[num / 100] + " hundred" + (num % 100 != 0 ? " and " + Cwords (num % 100) : "");
+            if (num < 1000000)
+                return Cwords (num / 1000) + " thousand" + (num % 1000 != 0 ? " " + Cwords (num % 1000) : "");
+            return "Number out of range";
+        }
+        static string ConvertToRoman (int num) {
+            if (num < 1 || num > 3999) {
+                return "Out of Roman numeral range";
+            }
+            string[] romansym = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            string romanNo = "";
+            for (int i = 0; i < values.Length; i++) {
+                while (num >= values[i]) { romanNo += romansym[i]; num -= values[i]; }
+            }
+            return romanNo;
+        }
         static void Main (string[] args) {
-            Console.Write ("Enter a decimal number : ");
-            int decino = int.Parse (Console.ReadLine ());
-            string hexadec = DecimalToHexadecimal (decino);
-            string binary = DecimalToBinary (decino);
-            Console.WriteLine ($"Hexadecimal:{hexadec}");
-            Console.WriteLine ($"Binary:{binary}");
-        }
-        static string DecimalToHexadecimal (int decino) {
-            string hex = "";
-            while (decino > 0) {
-                int rem = decino % 16;
-                hex = GetHexDigit (rem) + hex;
-                decino /= 16;
-            }
-            return hex;
-        }
-        static char GetHexDigit (int value) {
-            if (value >= 0 && value <= 9)
-                return (char)('0' + value);
-            else
-                return (char)('A' + (value - 10));
-        }
-        static string DecimalToBinary (int decino) {
-            string binary = "";
-            while (decino > 0) {
-                int rem = decino % 2;
-                binary = rem + binary;
-                decino /= 2;
-            }
-            return binary;
+            Console.Write ("Enter a number : ");
+            int num = int.Parse (Console.ReadLine ());
+            string words = Cwords (num);
+            Console.WriteLine ($"Number in words : {words}");
+            string roman = ConvertToRoman (num);
+            Console.WriteLine ($"Number in romans : {roman}");
         }
     }
 }
