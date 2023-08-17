1e0290718319067a0c16c5ddf67768f3e048563e
Harshini <harshini.s@metamation.com>
2023-08-17 14:21:47 +0530

    T05 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Training {
-    internal class Program {
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace primenumber {
+    class Program {
+        static bool IsPrime (int n) {
+            if (n <= 1) return false;
+            if (n <= 3) return true;
+            if (n % 2 == 0 || n % 3 == 0) return false;
+            for (int i = 5; i * i <= n; i += 6)
+                if (n % i == 0 || n % (i + 2) == 0) return false;
+            return true;
+        }
         static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
+            Console.Write ("Enter a number : ");
+            int n = int.Parse (Console.ReadLine ());
+            if (IsPrime (n))
+                Console.WriteLine ($"{n} is a prime number");
+            else
+                Console.WriteLine ($"{n} is not a prime number");
         }
     }
 }
