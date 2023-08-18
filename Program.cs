4eac3b96d7dceba821d9a22c1ede3afd19792405
Harshini <harshini.s@metamation.com>
2023-08-18 16:12:18 +0530

    Made changes in T06

--- Program.cs
+++ Program.cs
@@ -1,23 +1,21 @@
 ﻿using System;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace primenumber {
+namespace wordpalindrome {
     class Program {
-        static bool IsPrime (int n) {
-            if (n <= 1) return false;
-            if (n <= 3) return true;
-            if (n % 2 == 0 || n % 3 == 0) return false;
-            for (int i = 5; i * i <= n; i += 6)
-                if (n % i == 0 || n % (i + 2) == 0) return false;
+        static bool Palindrome (string words) {
+            string word = words.Replace (" ", "").ToLower ();
+            int left = 0, right = word.Length - 1;
+            while (left < right) {
+                if (word[left] != word[right]) return false;
+                left++; right--;
+            }
             return true;
         }
         static void Main (string[] args) {
-            Console.Write ("Enter a number : ");
-            int n = int.Parse (Console.ReadLine ());
-            if (IsPrime (n))
-                Console.WriteLine ($"{n} is a prime number");
-            else
-                Console.WriteLine ($"{n} is not a prime number");
+            Console.Write ("Enter a word or phrase : ");
+            string words = Console.ReadLine ();
+            if (Palindrome (words))
+                Console.WriteLine ("It is a Palindrome");
+            else Console.WriteLine ("It is not a Palindrome");
         }
     }
 }
