a0865cda979f0fcb074baf726ddfbb78305956a1
Harshini <harshini.s@metamation.com>
2023-08-21 14:52:19 +0530

    T07 completed

--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Linq;
+using System.Text;
 
-namespace primenumber {
+namespace chessboard {
     class Program {
-        static bool IsPrime (int n) {
-            if (n <= 1) return false;
-            if (n <= 3) return true;
-            if (n % 2 == 0 || n % 3 == 0) return false;
-            for (int i = 5; i * i <= n; i += 6)
-                if (n % i == 0 || n % (i + 2) == 0) return false;
-            return true;
-        }
         static void Main (string[] args) {
-            Console.Write ("Enter a number : ");
-            int n = int.Parse (Console.ReadLine ());
-            if (IsPrime (n))
-                Console.WriteLine ($"{n} is a prime number");
-            else
-                Console.WriteLine ($"{n} is not a prime number");
+            Console.OutputEncoding = new UnicodeEncoding ();
+            Console.WriteLine ("\u250c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u252c", 7)) + "\u2500\u2500\u2500\u2510");
+            Console.WriteLine ("\u2502 \u265c \u2502 \u265e \u2502 \u265d \u2502 \u265b \u2502 \u265a \u2502 \u265d \u2502 \u265e \u2502 \u265c \u2502");
+            Console.WriteLine ("\u251c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u253c", 7)) + "\u2500\u2500\u2500\u2524");
+            Console.WriteLine ("\u2502" + string.Concat (Enumerable.Repeat (" \u265f \u2502",8)));
+            for (int i = 0; i < 4; i++) {
+                Console.WriteLine ("\u251c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u253c", 7)) + "\u2500\u2500\u2500\u2524");
+                Console.WriteLine (string.Concat (Enumerable.Repeat ("\u2502   ", 9)));
+            } 
+            Console.WriteLine ("\u251c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u253c", 7)) + "\u2500\u2500\u2500\u2524");
+            Console.WriteLine ("\u2502" + string.Concat (Enumerable.Repeat (" \u2659 \u2502", 8)));
+            Console.WriteLine ("\u251c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u253c", 7)) + "\u2500\u2500\u2500\u2524");
+            Console.WriteLine ("\u2502 \u2656 \u2502 \u2658 \u2502 \u2657 \u2502 \u2655 \u2502 \u2654 \u2502 \u2657 \u2502 \u2658 \u2502 \u2656 \u2502");
+            Console.WriteLine ("\u2514" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u2534", 7)) + "\u2500\u2500\u2500\u2518");
         }
     }
 }
