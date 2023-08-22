687069a2cc7c2520bf7f4643341e556a38f29f9e
Harshini <harshini.s@metamation.com>
2023-08-22 14:42:31 +0530

    T10 completed

--- Program.cs
+++ Program.cs
@@ -1,21 +1,22 @@
 ﻿using System;
-namespace numbereverse {
-    class Program {
-        static void Main (string[] args) {
+namespace NumberReverse {
+   class Program {
+      static void Main (string[] args) {
+         while (true) {
             Console.Write ("Enter the number : ");
-            int n=int.Parse (Console.ReadLine());
-            int h=n;
-            int c = 0;
-            while(n>0) {
-                int r=n%10;
-                c = (c*10) + r;
-                n/=10;
-            }
-            Console.WriteLine ($"Reversed number : {c}");
-            if (h == c) {
-                Console.WriteLine ("It is a palindrome number");
-            } else
-                Console.WriteLine ("It is not a palindrome number");
-        }
-    }
+            if (int.TryParse (Console.ReadLine (), out int n)) {
+               int h = n;
+               int c = 0;
+               while (n > 0) {
+                  int r = n % 10;
+                  c = (c * 10) + r;
+                  n /= 10;
+               }
+               Console.WriteLine ($"Reversed number : {c}");
+               var res = (h == c) ? "It is a palindrome number" : "It is not a palindrome number";
+               Console.WriteLine (res); break;
+            } else Console.WriteLine ("Enter a valid number");
+         }
+      }
+   }
 }
