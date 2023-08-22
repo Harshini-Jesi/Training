61e75643855a8becf4dbb6c1fdfc1567cf16b6ca
Harshini <harshini.s@metamation.com>
2023-08-22 14:24:41 +0530

    T06 rewritten

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
-namespace wordpalindrome {
-    class Program {
-        static bool Palindrome (string words) {
-            string word = words.Replace (" ", "").ToLower ();
-            int left = 0, right = word.Length - 1;
-            while (left < right) {
-                if (word[left] != word[right]) return false;
-                left++; right--;
-            }
-            return true;
-        }
-        static void Main (string[] args) {
-            Console.Write ("Enter a word or phrase : ");
-            string words = Console.ReadLine ();
-            if (Palindrome (words))
-                Console.WriteLine ("It is a Palindrome");
-            else Console.WriteLine ("It is not a Palindrome");
-        }
-    }
+namespace WordPalindrome {
+   class Program {
+      static bool Palindrome (string words) {
+         string word = words.Replace (" ", "").ToLower ();
+         int left = 0, right = word.Length - 1;
+         while (left < right) {
+            if (word[left] != word[right]) return false;
+            left++; right--;
+         }
+         return true;
+      }
+
+      static void Main (string[] args) {
+         Console.Write ("Enter a word or phrase : ");
+         string words = Console.ReadLine ();
+         var res = Palindrome (words) ? "It is a Palindrome" : "It is not a Palindrome";
+         Console.WriteLine (res);
+      }
+   }
 }
