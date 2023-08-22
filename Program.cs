b4cf5c96a5aa3994daf551253ba27d30468883c8
Harshini <harshini.s@metamation.com>
2023-08-22 16:10:20 +0530

    T09 completed

--- Program.cs
+++ Program.cs
@@ -1,24 +1,17 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
-
-namespace chessboard {
-    class Program {
-        static void Main (string[] args) {
-            Console.OutputEncoding = new UnicodeEncoding ();
-            Console.WriteLine ("\u250c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u252c", 7)) + "\u2500\u2500\u2500\u2510");
-            Console.WriteLine ("\u2502 \u265c \u2502 \u265e \u2502 \u265d \u2502 \u265b \u2502 \u265a \u2502 \u265d \u2502 \u265e \u2502 \u265c \u2502");
-            Console.WriteLine ("\u251c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u253c", 7)) + "\u2500\u2500\u2500\u2524");
-            Console.WriteLine ("\u2502" + string.Concat (Enumerable.Repeat (" \u265f \u2502",8)));
-            for (int i = 0; i < 4; i++) {
-                Console.WriteLine ("\u251c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u253c", 7)) + "\u2500\u2500\u2500\u2524");
-                Console.WriteLine (string.Concat (Enumerable.Repeat ("\u2502   ", 9)));
-            } 
-            Console.WriteLine ("\u251c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u253c", 7)) + "\u2500\u2500\u2500\u2524");
-            Console.WriteLine ("\u2502" + string.Concat (Enumerable.Repeat (" \u2659 \u2502", 8)));
-            Console.WriteLine ("\u251c" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u253c", 7)) + "\u2500\u2500\u2500\u2524");
-            Console.WriteLine ("\u2502 \u2656 \u2502 \u2658 \u2502 \u2657 \u2502 \u2655 \u2502 \u2654 \u2502 \u2657 \u2502 \u2658 \u2502 \u2656 \u2502");
-            Console.WriteLine ("\u2514" + string.Concat (Enumerable.Repeat ("\u2500\u2500\u2500\u2534", 7)) + "\u2500\u2500\u2500\u2518");
-        }
-    }
+while (true) {
+   Console.Write ("Enter a number : ");
+   if (int.TryParse (Console.ReadLine (), out int n)) {
+      int spaces = n, stars = 1;
+      for (int i = 0; i <= n; i++) {
+         Console.WriteLine (string.Concat (Enumerable.Repeat (" ", spaces)) + string.Concat (Enumerable.Repeat ("*", stars)));
+         spaces -= 1; stars += 2;
+      }
+      spaces += 2; stars -= 4;
+      for (int i = 1; i <= n; i++) {
+         Console.WriteLine (string.Concat (Enumerable.Repeat (" ", spaces)) + string.Concat (Enumerable.Repeat ("*", stars)));
+         spaces += 1; stars -= 2;
+      }
+      break;
+   } else Console.WriteLine ("Enter a valid number");
 }
