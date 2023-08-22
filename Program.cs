29adc661efd14f4a83a02555f172ce6c6d9c692c
Harshini <harshini.s@metamation.com>
2023-08-22 14:06:01 +0530

    Made changes in T05

--- Program.cs
+++ Program.cs
@@ -2,19 +2,23 @@
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace primenumber {
-    class Program {
-        static bool IsPrime (int n) {
-            if (n <= 1) return false;
-            if (n <= 3) return true;
-            if (n % 2 == 0 || n % 3 == 0) return false;
-            for (int i = 5; i * i <= n; i += 6)
-                if (n % i == 0 || n % (i + 2) == 0) return false;
-            return true;
-        }
-        static void Main (string[] args) {
+   class Program {
+      static bool IsPrime (int n) {
+         if (n <= 1) return false;
+         if (n <= 3) return true;
+         if (n % 2 == 0 || n % 3 == 0) return false;
+         for (int i = 5; i * i <= n; i += 6)
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+         return true;
+      }
+      static void Main (string[] args) {
+         while (true) {
             Console.Write ("Enter a number : ");
-            int n = int.Parse (Console.ReadLine ());
-         var res => IsPrime (n) ? (Console.WriteLine ($"{n} is a prime number") : Console.WriteLine ($"{n} is not a prime number"));
-        }
-    }
+            if (int.TryParse (Console.ReadLine (), out int n)) {
+               var res = IsPrime (n) ? $"{n} is a prime number" : $"{n} is not a prime number";
+               Console.WriteLine (res); break;
+            } else Console.WriteLine ("Enter a valid number");
+         }
+      }
+   }
 }
