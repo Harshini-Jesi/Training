28fddd55f27bf09e5ebaea8a72941d0e4460925d
Harshini <harshini.s@metamation.com>
2023-08-22 11:36:05 +0530

    changes made in T03

--- Program.cs
+++ Program.cs
@@ -2,15 +2,19 @@
 namespace Fibonacci {
    class Program {
       static void Main (string[] args) {
-         Console.Write ("Enter a number : ");
-         int.TryParse (Console.ReadLine (), out int n);
-         Console.WriteLine ("Fibonacci series : ");
-         int first = 0, second = 1, next = 0;
-         for (int i = 0; i < n; i++) {
-            Console.WriteLine (first);
-            next = first + second;
-            first = second;
-            second = next;
+         while (true) {
+            Console.Write ("Enter a number : ");
+            if (int.TryParse (Console.ReadLine (), out int n)) {
+               Console.WriteLine ("Fibonacci series : ");
+               int first = 0, second = 1, next = 0;
+               for (int i = 0; i < n; i++) {
+                  Console.WriteLine (first);
+                  next = first + second;
+                  first = second;
+                  second = next;
+               }
+               break;
+            } else Console.WriteLine ("Enter a valid number");
          }
       }
    }
