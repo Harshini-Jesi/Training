9077c040ac4dc529b0a04d5f41968bc7039d17af
Harshini <harshini.s@metamation.com>
2023-08-22 12:31:46 +0530

    T04 rewritten

--- Program.cs
+++ Program.cs
@@ -1,26 +1,31 @@
 ﻿using System;
-namespace lcm_gcd {
-    class Program {
-        static void Main (string[] args) {
-            Console.Write ("Input 1 : ");
-            int n1 = int.Parse (Console.ReadLine ());
-            Console.Write ("Input 2 : ");
-            int n2 = int.Parse (Console.ReadLine ());
-            int gcd = GCD (n1, n2);
-            int lcm = LCM (n1, n2);
-            Console.WriteLine ($"GCD : {gcd}");
-            Console.WriteLine ($"LCM : {lcm}");
-        }
-        static int GCD (int x, int y) {
+namespace LcmGcd {
+   class Program {
+      static void Main (string[] args) {
+         while (true) {
+            Console.Write ("Enter first number : ");
+            if (int.TryParse (Console.ReadLine (), out int n1)) {
+               Console.Write ("Enter second number : ");
+               if (int.TryParse (Console.ReadLine (), out int n2)) {
+                  int gcd = GCD (n1, n2), lcm = LCM (n1, n2);
+                  Console.WriteLine ($"GCD : {gcd}");
+                  Console.WriteLine ($"LCM : {lcm}"); break;
+               } else Console.WriteLine ("Enter a valid number");
+            } else Console.WriteLine ("Enter a valid number");
+         }
+
+         static int GCD (int x, int y) {
             while (y != 0) {
-                int rem = x % y;
-                x = y;
-                y = rem;
+               int rem = x % y;
+               x = y;
+               y = rem;
             }
             return x;
-        }
-        static int LCM (int x, int y) {
+         }
+
+         static int LCM (int x, int y) {
             return (x * y) / GCD (x, y);
-        }
-    }
+         }
+      }
+   }
 }
