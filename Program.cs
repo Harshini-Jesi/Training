210f1c50e1706c760af33698bd5fe7c0440af2e8
Harshini <harshini.s@metamation.com>
2023-08-22 10:19:18 +0530

    Made changes in T03

--- Program.cs
+++ Program.cs
@@ -1,22 +1,17 @@
 ﻿using System;
 namespace Fibonacci {
-    class Program {
-        static void Main (string[] args) {
-            Console.Write ("Input : ");
-            int n = int.Parse (Console.ReadLine ());
-            Console.Write ("Fibonacci series : ");
-            for (int i = 0; i < n; i++) { Console.Write (fib (i) + " "); }
-        }
-        static int fib (int n) {
-            if (n <= 1)
-                return n;
-            int fib1 = 0, fib2 = 1, fib3 = 0;
-            for (int i = 2; i <= n; i++) {
-                fib3 = fib1 + fib2;
-                fib1 = fib2;
-                fib2 = fib3;
-            }
-            return fib3;
-        }
-    }
+   class Program {
+      static void Main (string[] args) {
+         Console.Write ("Enter a number : ");
+         int.TryParse (Console.ReadLine (), out int n);
+         Console.WriteLine ("Fibonacci series : ");
+         int first = 0, second = 1, next = 0;
+         for (int i = 0; i < n; i++) {
+            Console.WriteLine (first);
+            next = first + second;
+            first = second;
+            second = next;
+         }
+      }
+   }
 }
