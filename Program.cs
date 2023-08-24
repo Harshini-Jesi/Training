dd087aa1ea5fcff32f680c230f7724a401d3e556
Harshini <harshini.s@metamation.com>
2023-08-24 10:30:31 +0530

    T05 corrected

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ namespace PrimeNumber {
          if (n <= 1) return false;
          if (n <= 3) return true;
          if (n % 2 == 0 || n % 3 == 0) return false;
-         for (int i = 5; i * i <= n; i += 6)
-            if (n % i == 0 || n % (i + 2) == 0) return false;
+         for (int i = 5; i * i <= n; i += 6)//To see if the number can represented as 6k+1 or 6k-1
+            if (n % i == 0 || n % (i + 2) == 0) return false;//i checks for 6k+1 and i+2 checks for 6k-1
          return true;
       }
 
