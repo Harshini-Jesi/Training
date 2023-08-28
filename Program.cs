198b1d97fb03f9d7101cf34242ecf916a76cd40e
Harshini <harshini.s@metamation.com>
2023-08-28 09:45:33 +0530

    T04 changes done

--- Program.cs
+++ Program.cs
@@ -1,31 +1,43 @@
-﻿using System;
-namespace LcmGcd {
-   class Program {
-      static void Main (string[] args) {
-         while (true) {
-            Console.Write ("Enter first number : ");
-            if (int.TryParse (Console.ReadLine (), out int n1)) {
-               Console.Write ("Enter second number : ");
-               if (int.TryParse (Console.ReadLine (), out int n2)) {
-                  int gcd = GCD (n1, n2), lcm = LCM (n1, n2);
-                  Console.WriteLine ($"GCD : {gcd}");
-                  Console.WriteLine ($"LCM : {lcm}"); break;
-               } else Console.WriteLine ("Enter a valid number");
-            } else Console.WriteLine ("Enter a valid number");
-         }
-
-         static int GCD (int x, int y) {
-            while (y != 0) {
-               int rem = x % y;
-               x = y;
-               y = rem;
-            }
-            return x;
-         }
-
-         static int LCM (int x, int y) {
-            return (x * y) / GCD (x, y);
-         }
+﻿while (true) {
+   Console.Write ("Enter the number of inputs for LCM and GCD : ");
+   if (int.TryParse (Console.ReadLine (), out int n)) {
+      while (true) {
+         Console.Write ($"Enter {n} numbers leaving a space between them : ");
+         string numbers = Console.ReadLine ();
+         int[] values = numbers.Split (' ').Select (int.Parse).ToArray ();
+         if (values.Length == n) {
+            int gcd = GCD (values);
+            int lcm = LCM (values);
+            Console.WriteLine ($"\nGCD : {gcd}");
+            Console.WriteLine ($"LCM : {lcm}"); break;
+         } else Console.WriteLine ($"Enter '{n}' numbers.\n");
       }
-   }
+   } else Console.WriteLine ("Invalid input. Please enter a 'NUMBER'.\n"); break;
 }
+
+static int FindGCD (int x, int y) {
+   while (y != 0) {
+      int rem = x % y;
+      x = y;
+      y = rem;
+   }
+   return x;
+}
+
+static int FindLCM (int x, int y) {
+   return (x * y) / FindGCD (x, y);
+}
+
+static int GCD (int[] values) {
+   int gcd = values[0];
+   for (int i = 1; i < values.Length; i++) gcd = FindGCD (gcd, values[i]);
+   return gcd;
+}
+
+static int LCM (int[] values) {
+   int lcm = values[0];
+   for (int i = 1; i < values.Length; i++) lcm = FindLCM (lcm, values[i]);
+   return lcm;
+}
+
+
