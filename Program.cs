8c148ab8927a5aeddbcd17cd54b5a049dc6aa060
Harshini <harshini.s@metamation.com>
2023-08-28 12:53:25 +0530

    T02 changes done

--- Program.cs
+++ Program.cs
@@ -7,21 +7,22 @@ namespace NumberConversion {
       static string[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
 
       static string ConvertToWords (int num) {
+         int rem10 = num % 10, rem100 = num % 100, rem1000 = num % 1000;
          if (num < 0) return ("Negative " + ConvertToWords (-num));
          if (num < 20) return ones[num];
-         if (num < 100) return tens[num / 10] + (num % 10 != 0 ? " " + ones[num % 10] : " ");
-         if (num < 1000) return ones[num / 100] + " hundred" + (num % 100 != 0 ? " and " + ConvertToWords (num % 100) : "");
-         if (num < 1000000) return ConvertToWords (num / 1000) + " thousand" + (num % 1000 != 0 ? " " + ConvertToWords (num % 1000) : "");
+         if (num < 100) return tens[num / 10] + (rem10 != 0 ? " " + ones[rem10] : " ");
+         if (num < 1000) return ones[num / 100] + " hundred" + (rem100 != 0 ? " and " + ConvertToWords (rem100) : "");
+         if (num < 1000000) return ConvertToWords (num / 1000) + " thousand" + (rem1000 != 0 ? " " + ConvertToWords (rem1000) : "");
          return "Number out of range";
       }
 
       static string ConvertToRoman (int num) {
          if (num < 1 || num > 3999) return "Out of Roman numeral range";
-         string[] romansym = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+         string[] romanSym = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
          int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
          string romanNo = "";
          for (int i = 0; i < values.Length; i++) {
-            while (num >= values[i]) { romanNo += romansym[i]; num -= values[i]; }
+            while (num >= values[i]) { romanNo += romanSym[i]; num -= values[i]; }
          }
          return romanNo;
       }
@@ -33,8 +34,9 @@ namespace NumberConversion {
                string words = ConvertToWords (num);
                Console.WriteLine ($"Number in words : {words}");
                string roman = ConvertToRoman (num);
-               Console.WriteLine ($"Number in romans : {roman}");break;
-            } else Console.WriteLine ("Enter a valid number");
+               Console.WriteLine ($"Number in romans : {roman}");
+               break;
+            } else Console.WriteLine ("Invalid input. Please enter a valid 'NUMBER'.\n");
          }
       }
    }
