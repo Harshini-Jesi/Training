06278977893fa3ab3918dda4e2667cfa89ce2d2e
Harshini <harshini.s@metamation.com>
2023-08-28 15:29:39 +0530

    A02 modified

--- Program.cs
+++ Program.cs
@@ -1,28 +1,12 @@
-﻿var mode = GetMode (); Console.WriteLine (mode);
-int max = GetMax (mode);
+﻿Console.Write ("Select a mode  (E)asy, (M)edium, (H)ard : ");
+var mode = GetMode (); Console.WriteLine (mode);
+int max = mode switch {
+   Mode.Easy => 10,
+   Mode.Medium => 100,
+   _ => 1000
+};
 Random random = new Random ();
 int target = random.Next (1, max + 1);
-
-Mode GetMode () {
-   Console.Write ("Select a mode  (E)asy, (M)edium, (H)ard : ");
-   for (; ; ) {
-      var key = Console.ReadKey (true).Key;
-      switch (key) {
-         case ConsoleKey.E: return Mode.Easy;
-         case ConsoleKey.M: return Mode.Medium;
-         case ConsoleKey.H: return Mode.Hard;
-         default: Console.WriteLine ("Incorrect key\n"); break;
-      }
-   }
-}
-
-int GetMax (Mode mode) {
-   switch (mode) {
-      case Mode.Easy: return 10;
-      case Mode.Medium: return 100;
-      default: return 1000;
-   }
-}
 Console.WriteLine ($"Guess a number between 1 and {max}\n");
 int tries = 0, n = 0;
 while (n != target) {
@@ -34,4 +18,18 @@ while (n != target) {
       if (n == target) Console.WriteLine ($"\nYou guessed the number in {tries} tries");
    } else Console.WriteLine ("Invalid input. Please enter a valid 'NUMBER'\n");
 }
+
+Mode GetMode () {
+   for (; ; ) {
+      var key = Console.ReadKey (true).Key;
+      switch (key) {
+         case ConsoleKey.E: return Mode.Easy;
+         case ConsoleKey.M: return Mode.Medium;
+         case ConsoleKey.H: return Mode.Hard;
+         default: Console.WriteLine ("Incorrect key\n");
+            break;
+      }
+   }
+}
+
 enum Mode { Easy, Medium, Hard };
