5279501c9f1bb4123f723e2498d0a9870e7b5c69
Harshini <harshini.s@metamation.com>
2023-08-28 12:42:54 +0530

    T12 changes done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿while (true) {
    Console.Write ("Enter the number of rows : ");
    if (int.TryParse (Console.ReadLine (), out int n)) {
+      Console.WriteLine ("Pascal's Triangle");
       for (int i = 1; i <= n; i++) {
-         Console.Write (string.Concat (Enumerable.Repeat (" ", n - i + 1)));
+         Console.Write (new string(' ', n - i + 1));
          int first = 1;
          for (int j = 1; j <= i; j++) {
-            Console.Write ($"{first,5}" + " ");
+            Console.Write ($"{first}  ");
             first = first * (i - j) / j;
          }
          Console.WriteLine ("\n");
       }
       break;
-   } else Console.WriteLine ("Invalid input. Please enter a 'NUMBER'");
+   } else Console.WriteLine ("Invalid input. Please enter a 'NUMBER'.\n");
 }
