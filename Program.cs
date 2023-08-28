4a6462a58fbad8d5a7976c53b48a643c5b0e169d
Harshini <harshini.s@metamation.com>
2023-08-28 09:11:42 +0530

    T09 changes done

--- Program.cs
+++ Program.cs
@@ -3,9 +3,8 @@ while (true) {
    Console.Write ("Enter a number : ");
    if (int.TryParse (Console.ReadLine (), out int n)) {
       int spaces = n, stars = 1;
-      for (int i = 0; i <= n; i++,stars+=2) Console.WriteLine (string.Concat (Enumerable.Repeat (" ", spaces--)) + string.Concat (Enumerable.Repeat ("*", stars)));
+      for (int i = 0; i <= n; i++, stars += 2) Console.WriteLine ($"{new string (' ', spaces--)}{new string ('*', stars)}");
       spaces += 2; stars -= 4;
-      for (int i = 1; i <= n; i++,stars-=2) Console.WriteLine (string.Concat (Enumerable.Repeat (" ", spaces++)) + string.Concat (Enumerable.Repeat ("*", stars)));
-      break;
+      for (int i = 1; i <= n; i++, stars -= 2) Console.WriteLine ($"{new string (' ', spaces++)}{new string ('*', stars)}"); break;
    } else Console.WriteLine ("Invalid input. Please enter a 'NUMBER'\n");
 }
