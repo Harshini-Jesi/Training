25bb33f1deff64d812ded3481fa8efbc96895a5b
Harshini <harshini.s@metamation.com>
2023-08-29 10:36:26 +0530

    T12 modified

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿while (true) {
+﻿using System.Xml.Serialization;
+
+while (true) {
    Console.Write ("Enter the number of rows : ");
    if (int.TryParse (Console.ReadLine (), out int n)) {
       Console.WriteLine ("Pascal's Triangle");
-      for (int i = 1; i <= n; i++) {
-         Console.Write (new string(' ', n - i + 1));
+      int space = n * 2;
+      for (int i = 1; i <= n; i++, space -= 2) {
+         Console.Write (new string (' ', space));
          int first = 1;
          for (int j = 1; j <= i; j++) {
-            Console.Write ($"{first}  ");
+            Console.Write ($"{first}   ");
             first = first * (i - j) / j;
          }
          Console.WriteLine ("\n");
