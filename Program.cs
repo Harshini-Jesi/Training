5d336fb350d109b2a7bf4d08eef3cc301b245496
Harshini <harshini.s@metamation.com>
2023-08-30 14:16:36 +0530

    T13 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿while (true) {
+   Console.Write ("Enter your password : ");
+   string password = Console.ReadLine ();
+   string spclChar = "!@#$%^&*()-+";
+   bool output = password.Length >= 6 && password.Any (char.IsDigit) && password.Any (char.IsLower) && password.Any (char.IsUpper) && password.Any (spclChar.Contains);
+   if (output == true) { Console.WriteLine ("Your password is strong."); break; } 
+   else {
+      Console.WriteLine ("Your password is not strong.");
+      if (password.Length < 6) Console.WriteLine ("Length of the password must be atleast 6.");
+      if (password.Any (char.IsDigit) == false) Console.WriteLine ("Password must contain atleast one digit.");
+      if (password.Any (char.IsLower) == false) Console.WriteLine ("Password must contain atleast one lowercase.");
+      if (password.Any (char.IsUpper) == false) Console.WriteLine ("Password must contain atleast one Uppercase.");
+      if (password.Any (spclChar.Contains) == false) Console.WriteLine ("Password must contain atleast one Special character.");
+      Console.WriteLine ("\n");
+   }
 }
