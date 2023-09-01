ae560517fe5751f5621399181b394bc425f56e30
Harshini <harshini.s@metamation.com>
2023-09-01 16:56:28 +0530

    T15 in progress

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿while (true) {
+   Console.Write ("Enter the string of lowercase : ");
+   string input = Console.ReadLine ();
+   if (input.All (char.IsLower)) {
+      var res = input.Distinct ().Count () == input.Length ? "It's an isogram" : "It's not an isogram";
+      Console.WriteLine (res);
+      break;
+   } else Console.WriteLine ("Enter the string in lowercase.\n");
 }
