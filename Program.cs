ed72ed4427f87a4d67f113a187794dbe02940018
Harshini <harshini.s@metamation.com>
2023-09-01 15:03:35 +0530

    T14 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿using System.Text;
+while (true) {
+   Console.Write ("Enter the string of lowercase : ");
+   string input = Console.ReadLine ();
+   if (input.All (char.IsLower)) {
+      StringBuilder output = new StringBuilder ();
+      foreach (char c in input) {
+         var function = output.Length > 0 && output[output.Length - 1] == c ? output.Remove (output.Length - 1, 1) : output.Append (c);
+      }
+      var res = output.Length==0 ? "Reduced string is 'Empty'" : $"Reduced string is '{output}'";
+      Console.WriteLine (res);
+      break;
+   } else Console.WriteLine ("Enter the string in lowercase.\n");
 }
