cba6304d72039e36b1386a1562fb8111c725dd14
Harshini <harshini.s@metamation.com>
2023-09-04 11:53:21 +0530

    in process

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿using System.Text;
 while (true) {
-   Console.Write ("Enter the string of lowercase : ");
+   Console.Write ("Enter the string : ");
    string input = Console.ReadLine ();
-   if (input.All (char.IsLower)) {
+   if (input.All (char.IsLetter)) {
+      input = input.ToLower ();
       StringBuilder output = new StringBuilder ();
       foreach (char c in input) {
-         var function = output.Length > 0 && output[output.Length - 1] == c ? output.Remove (output.Length - 1, 1) : output.Append (c);
+         if (output.Length > 0 && output[output.Length - 1] == c) output.Remove (output.Length - 1, 1);
+         else output.Append (c);
       }
-      var res = output.Length==0 ? "Reduced string is 'Empty'" : $"Reduced string is '{output}'";
+      var res = output.Length == 0 ? "Reduced string is 'Empty'" : $"Reduced string is '{output}'";
       Console.WriteLine (res);
       break;
-   } else Console.WriteLine ("Enter the string in lowercase.\n");
+   } else Console.WriteLine ("The string should consist of letters only.\n");
 }
