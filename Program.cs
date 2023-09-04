39c7ffc28938cd1765732a3f4f8496ae56f7a1ac
Harshini <harshini.s@metamation.com>
2023-09-04 16:41:46 +0530

    Created new T15 branch

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿while (true) {
+   Console.Write ("Enter the string in lowercase: ");
+   string input = Console.ReadLine ();
+   if (!input.All (char.IsLower)) continue;
+   Console.WriteLine (input.Distinct ().Count () == input.Length ? "It's an isogram" : "It's not an isogram");
+   break;
 }
