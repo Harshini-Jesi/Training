fbcc9af12042619cdeaaf505d1b67ea620873bfe
Harshini-Jesi <141300734+Harshini-Jesi@users.noreply.github.com>
2023-09-04 14:04:29 +0530

    T15 modified

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
    Console.Write ("Enter the string in lowercase: ");
    string input = Console.ReadLine ();
    if (!input.All (char.IsLower)) continue;
-   Console.WriteLine(input.Distinct ().Count () == input.Length ? "It's an isogram" : "It's not an isogram");
+   Console.WriteLine (input.Distinct ().Count () == input.Length ? "It's an isogram" : "It's not an isogram");
    break;
 }
