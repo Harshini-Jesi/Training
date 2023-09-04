23d2da9a17449b5ca4130158e8f4698d22c5b063
Harshini <harshini.s@metamation.com>
2023-09-04 13:09:14 +0530

    T15 modified

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿while (true) {
-   Console.Write ("Enter the string of lowercase : ");
-   string input = Console.ReadLine ();
-   if (input.All (char.IsLower)) {
-      var res = input.Distinct ().Count () == input.Length ? "It's an isogram" : "It's not an isogram";
-      Console.WriteLine (res);
-      break;
-   } else Console.WriteLine ("Enter the string in lowercase.\n");
-}
---
+++ training/Program.cs
@@ -0,0 +1,7 @@
+﻿while (true) {
+   Console.Write ("Enter the string in lowercase: ");
+   string input = Console.ReadLine ();
+   if (!input.All (char.IsLower)) continue;
+   Console.WriteLine(input.Distinct ().Count () == input.Length ? "It's an isogram" : "It's not an isogram");
+   break;
+}
