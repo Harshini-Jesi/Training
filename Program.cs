aee03caa2e5759d136c9ea4e885804cc3f2e6e24
Harshini <harshini.s@metamation.com>
2023-09-04 12:48:46 +0530

    in process

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿while (true) {
+   Console.Write ("Enter the number of input words : ");
+   if (int.TryParse (Console.ReadLine (), out int n)) {
+      while (true) {
+         Console.Write ($"Enter {n} words leaving a space between them : ");
+         string enteredWords = Console.ReadLine ();
+         string[] allWords = enteredWords.Split (' ').ToArray ();
+         List<string> alignedWords = new ();
+         if (allWords.Length == n) {
+            foreach (string word in allWords) {
+               char[] chars = word.ToCharArray ();
+               Array.Sort (chars);
+               if (new string (chars) == word) alignedWords.Add (word);
+            }
+            var sort = alignedWords.OrderBy (word => word.Length);
+            var res = alignedWords.Count == 0 ? "" : $"The longest abecedarian word is {sort.LastOrDefault ()}";
+            Console.WriteLine (res);
+            break;
+         } else Console.WriteLine ($"Enter '{n}' words.\n");
+      }
+   } else Console.WriteLine ("Invalid input. Please enter a 'NUMBER'.\n");
+   break;
 }
