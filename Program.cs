9bcc02302c680766653a72d47cd10e7bbfc05e83
Harshini <harshini.s@metamation.com>
2023-09-05 12:18:07 +0530

    T13 modified

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@
    if (password.Length < 6) a += "Length of the password must be atleast 6.\n";
    if (!password.Any (char.IsDigit)) a += "Password must contain atleast one digit.\n";
    if (!password.Any (char.IsLower)) a += "Password must contain atleast one lowercase.\n";
-   if (!password.Any (char.IsUpper)) a += "Password must contain atleast one Uppercase.\n";
-   if (!password.Any (spclChar.Contains)) a += "Password must contain atleast one Special character.\n";
+   if (!password.Any (char.IsUpper)) a += "Password must contain atleast one uppercase.\n";
+   if (!password.Any (spclChar.Contains)) a += "Password must contain atleast one special character.\n";
    if (string.IsNullOrEmpty (a)) {
       Console.WriteLine ("Your password is strong.");
       break;
