313234f8ff6cd9c90e0dd60ae255812b0094744d
Harshini <harshini.s@metamation.com>
2023-09-06 15:08:55 +0530

    T16 completed

--- Program.cs
+++ Program.cs
@@ -1,23 +1,20 @@
 ﻿while (true) {
    Console.Write ("Enter the number of input words : ");
-   if (int.TryParse (Console.ReadLine (), out int n)) {
-      while (true) {
-         Console.Write ($"Enter {n} words leaving a space between them : ");
-         string enteredWords = Console.ReadLine ();
-         string[] allWords = enteredWords.Split (' ').ToArray ();
-         List<string> alignedWords = new ();
-         if (allWords.Length == n) {
-            foreach (string word in allWords) {
-               char[] chars = word.ToCharArray ();
-               Array.Sort (chars);
-               if (new string (chars) == word) alignedWords.Add (word);
-            }
-            var sort = alignedWords.OrderBy (word => word.Length);
-            var res = alignedWords.Count == 0 ? "" : $"The longest abecedarian word is {sort.LastOrDefault ()}";
-            Console.WriteLine (res);
-            break;
-         } else Console.WriteLine ($"Enter '{n}' words.\n");
+   if (!int.TryParse (Console.ReadLine (), out int n)) continue;
+   while (true) {
+      Console.Write ($"Enter {n} words leaving a space between them : ");
+      string enteredWords = Console.ReadLine ();
+      string[] allWords = enteredWords.Split (' ').ToArray ();
+      if (allWords.Length != n) continue;
+      List<string> alignedWords = new ();
+      foreach (string word in allWords) {
+         char[] chars = word.ToCharArray ();
+         Array.Sort (chars);
+         if (new string (chars) == word) alignedWords.Add (word);
       }
-   } else Console.WriteLine ("Invalid input. Please enter a 'NUMBER'.\n");
+      var sort = alignedWords.OrderBy (word => word.Length);
+      Console.WriteLine (alignedWords.Count == 0 ? "No abecedarian word." : $"The longest abecedarian word is {sort.LastOrDefault ()}.");
+      break;
+   }
    break;
 }
