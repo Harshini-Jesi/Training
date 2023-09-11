64a4751fa835059b85405f3c1301da2391825813
Harshini <harshini.s@metamation.com>
2023-09-11 10:46:52 +0530

    Made changes in T20

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
-﻿using System.Text.RegularExpressions;
-
-while (true) {
+﻿while (true) {
    Console.Write ("Enter a decimal number: ");
    string input = Console.ReadLine ();
-   if (input.All (char.IsDigit)) {
-      Console.WriteLine ($"integral part: {AddSpaces (input)}\nfactorial part: There is no factorial part."); break;
-   } else if (Regex.IsMatch (input, @"^[0-9]+\.[0-9]+$")) {
+   if (!decimal.TryParse (input, out decimal result)) continue;
+   if (input.Contains ('.')) {
       string[] number = input.Split ('.').ToArray ();
-      Console.WriteLine ($"integral part: {AddSpaces (number[0])}\nfactorial part: {AddSpaces (number[1])}"); break;
-   } else continue;
+      Console.WriteLine ($"integral part: {ToSpaceSeparatedString (number[0])}\nfactorial part: {ToSpaceSeparatedString (number[1])}"); break;
+   } else Console.WriteLine ($"integral part: {ToSpaceSeparatedString (input)}\nfactorial part: There is no factorial part."); break;
 }
 
-static string AddSpaces (string input) {
+static string ToSpaceSeparatedString (string input) {
    string spacedString = string.Join (" ", input.Select (c => c));
    return spacedString;
 }
