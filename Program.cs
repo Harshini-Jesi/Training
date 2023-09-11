e02870dac0f949d7019426b6422479c0124083ec
Harshini <harshini.s@metamation.com>
2023-09-11 15:38:33 +0530

    T21 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿while (true) {
+   Console.WriteLine ("Assign numbers for a and b");
+   Console.Write ("a = ");
+   if (!int.TryParse (Console.ReadLine (), out int a)) continue;
+   Console.Write ("b = ");
+   if (!int.TryParse (Console.ReadLine (), out int b)) continue;
+   Console.WriteLine ($"After swapping: a = {swap (b, a)}, b = {swap (a, b)}");
+   break;
+}
+
+static int swap (int a, int b) {
+   int temp = a;
+   a = b;
+   b = temp;
+   return temp;
 }
