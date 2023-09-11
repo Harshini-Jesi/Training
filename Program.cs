7d27d312564b8de071cd1b5a79a0991a9201a383
Harshini <harshini.s@metamation.com>
2023-09-11 12:01:02 +0530

    T20 modified

--- Program.cs
+++ Program.cs
@@ -2,10 +2,11 @@
    Console.Write ("Enter a decimal number: ");
    string input = Console.ReadLine ();
    if (!decimal.TryParse (input, out decimal result)) continue;
-   if (input.Contains ('.')) {
-      string[] number = input.Split ('.').ToArray ();
-      Console.WriteLine ($"integral part: {ToSpaceSeparatedString (number[0])}\nfactorial part: {ToSpaceSeparatedString (number[1])}"); break;
-   } else Console.WriteLine ($"integral part: {ToSpaceSeparatedString (input)}\nfactorial part: There is no factorial part."); break;
+   string[] splits = input.Split ('.').ToArray ();
+   Console.WriteLine ($"integral part: {ToSpaceSeparatedString (splits[0])}");
+   var s = splits.Length > 1 ? ToSpaceSeparatedString (splits[1]) : "There is no factorial part.";
+   Console.WriteLine ($"factorial part: {s}");
+   break;
 }
 
 static string ToSpaceSeparatedString (string input) {
