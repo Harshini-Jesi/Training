5f5917ac6c71783f7661e1f728ca405f08e31458
Harshini <harshini.s@metamation.com>
2023-09-12 11:03:57 +0530

    T21 modified

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-﻿while (true) {
-   Console.WriteLine ("Assign numbers for a and b");
-   Console.Write ("a = ");
-   if (!int.TryParse (Console.ReadLine (), out int a)) continue;
-   Console.Write ("b = ");
-   if (!int.TryParse (Console.ReadLine (), out int b)) continue;
-   Console.WriteLine ($"After swapping: a = {swap (b, a)}, b = {swap (a, b)}");
-   break;
+﻿Console.WriteLine ("Assign numbers for a and b");
+int a = GetInput ("a = "), b = GetInput ("b = ");
+swap (ref a, ref b);
+Console.WriteLine ($"After swapping: a = {a}, b = {b}");
+
+int GetInput (string prompt) {
+   while (true) {
+      Console.Write (prompt);
+      if (int.TryParse (Console.ReadLine (), out int value)) return value;
+      else continue;
+   }
 }
 
-static int swap (int a, int b) {
-   int temp = a;
-   a = b;
-   b = temp;
-   return temp;
+void swap (ref int a, ref int b) {
+   (a, b) = (b, a);
 }
