4bd1fd4837639018ca12ee90eda500fe9f47f056
Harshini <harshini.s@metamation.com>
2023-09-12 17:06:17 +0530

    T22-swap indices

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+﻿int number = GetInput ("Enter a number : ");
+char[] value = Convert.ToString (number).ToCharArray ();
+Console.WriteLine ("Enter the index positions to be swapped:");
+int a = GetInput ("FromPosition = "), b = GetInput ("ToPosition = ");
+swap (value, ref a, ref b);
+Console.WriteLine ($"Index swapped number: {new string (value)}");
+
+int GetInput (string prompt) {
+   while (true) {
+      Console.Write (prompt);
+      if (int.TryParse (Console.ReadLine (), out int value)) return value;
+      else continue;
+   }
 }
+
+void swap (char[] value, ref int a, ref int b) => (value[b], value[a]) = (value[a], value[b]);
