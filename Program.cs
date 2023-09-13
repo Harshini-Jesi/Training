a3bedbf20e09bb44466e8cd8baa99769194f0a72
Harshini <harshini.s@metamation.com>
2023-09-13 16:47:14 +0530

    T22 modified

--- Program.cs
+++ Program.cs
@@ -1,18 +1,23 @@
-﻿int number = GetInput ("Enter a number : ");
+﻿int number = GetInput ("Enter a positive number : ");
 char[] value = Convert.ToString (number).ToCharArray ();
+Console.WriteLine ($"Enter the index positions to be swapped within 0 to {value.Length - 1}:");
 while (true) {
-   Console.WriteLine ("Enter the index positions to be swapped:");
-   int a = GetInput ("FromPosition = "), b = GetInput ("ToPosition = ");
-   if (a <= value.Length && b <= value.Length) {
-      swap (value, a, b);
-      Console.WriteLine ($"Index swapped number: {new string (value)}");
-      break;
-   } else Console.WriteLine ($"Enter index positions within {value.Length - 1}\n");
+   int a = GetInput ("FromPosition = ");
+   if (a < value.Length) {
+      while (true) {
+         int b = GetInput ("ToPosition = ");
+         if (b < value.Length) {
+            swap (value, a, b);
+            Console.WriteLine ($"Index swapped number: {new string (value)}");
+            break;
+         } else continue;
+      }break;
+   } else continue;
 }
 int GetInput (string prompt) {
    while (true) {
       Console.Write (prompt);
-      if (int.TryParse (Console.ReadLine (), out int value)) return value;
+      if (int.TryParse (Console.ReadLine (), out int value) && value >= 0) return value;
       else continue;
    }
 }
