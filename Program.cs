4740e16a62268d9f4b7d936616191650cf59297d
Sivabalan <Harshini.Sivabalan@trumpf.com>
2023-09-25 17:22:58 +0530

    Made changes in T22

--- Program.cs
+++ Program.cs
@@ -1,27 +1,24 @@
-﻿int number = GetInput ("Enter a positive number : ");
-char[] value = Convert.ToString (number).ToCharArray ();
-Console.WriteLine ($"Enter the index positions to be swapped within 0 to {value.Length - 1}:");
-while (true) {
-   int a = GetInput ("FromPosition = ");
-   if (a < value.Length) {
-      while (true) {
-         int b = GetInput ("ToPosition = ");
-         if (b < value.Length) {
-            swap (value, a, b);
-            Console.WriteLine ($"Index swapped number: {new string (value)}");
-            break;
-         } else continue;
-      }
-      break;
-   } else continue;
+﻿int number = GetInput("Enter a positive number : ");
+char[] arr = Convert.ToString(number).ToCharArray();
+int max = arr.Length - 1;
+Console.WriteLine($"Enter the index positions to be swapped within 0 to {max}:");
+int from = GetInput("FromPosition = ", max);
+int to = GetInput("ToPosition = ", max);
+Swap(arr, from, to);
+Console.WriteLine($"Index swapped number: {new string(arr)}");
+
+///<summary>Gets the input from the user until it is a positive number</summary>
+///<param name="prompt">Displays the prompt for the user to enter the input</param>
+///<param name="max">if set to -1 no range check is done, otherwise checks if value is less than or equal to max</param>
+int GetInput(string prompt, int max = -1)
+{
+    while (true)
+    {
+        Console.Write(prompt);
+        if (!int.TryParse(Console.ReadLine(), out int value) || value < 0) continue;
+        if (max == -1 || value <= max) return value;
+    }
 }
 
-int GetInput (string prompt) {
-   while (true) {
-      Console.Write (prompt);
-      if (int.TryParse (Console.ReadLine (), out int value) && value >= 0) return value;
-      else continue;
-   }
-}
+void Swap(char[] arr, int from, int to) => (arr[to], arr[from]) = (arr[from], arr[to]);
 
-void swap (char[] value, int a, int b) => (value[b], value[a]) = (value[a], value[b]);
