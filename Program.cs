87d5b0cbcfff24ec19d7e6a84a7a33fbbf134941
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2023-09-28 16:03:04 +0530

    Made changes in T18

--- Program.cs
+++ Program.cs
@@ -4,24 +4,23 @@ class Program {
    static void Main () {
       while (true) {
          Console.Write ("Enter a positive number: ");
-         if (!int.TryParse (Console.ReadLine (), out int number) && number > 0) continue;
-         int originalNumber = number;
-         Console.WriteLine (originalNumber == Armstrong (number) ? "It is an Armstrong number" : "Not an Armstrong number");
+         if (int.TryParse (Console.ReadLine (), out int number) && number > 0) Console.WriteLine (IsArmstrong (number) ? "It is an Armstrong number" : "Not an Armstrong number");
+         else continue;
          break;
       }
    }
 
    /// <summary>Calculates the sum of nth power of each digit, where n is the length of the given number</summary>
    /// <param name="number">The number to be checked whether armstrong or not</param>
-   /// <returns>The sum of nth power of each digit</returns>
-   static double Armstrong (int number) {
+   /// <returns><c>true</c>if the given number and the sum of nth power of each digit are same;<c>false</c>otherwise</returns>
+   static bool IsArmstrong (int number) {
+      int originalNumber = number;
       int power = number.ToString ().Length;
-      double sum = 0;
+      int sum = 0;
       for (int i = 0; i < power; i++) {
-         int temp = number % 10;
-         sum += Math.Pow (temp, power);
+         sum += (int)Math.Pow (number % 10, power);
          number /= 10;
       }
-      return sum;
+      return originalNumber == sum;
    }
 }
