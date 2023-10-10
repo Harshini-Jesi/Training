241eda0a754d2c88e1766ab6a1509b1332e28029
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2023-10-10 16:18:04 +0530

    Made changes in T25

--- Program.cs
+++ Program.cs
@@ -2,39 +2,29 @@
 /// and returns the sorted array by keeping the elements matching S to the last of the array.</summary>
 class Program {
    static void Main () {
-      string result = "", result1 = "", result2 = "";
       char[] arr = CharArray ();
       char spclChar = SpecialChar ();
-      foreach (char c in arr) {
-         if (c != spclChar) result1 += c;
-         else result2 += c;
-      }
       Console.WriteLine ("Enter 'y' if you want to sort in descending: ");
       var key = Console.ReadKey (true).Key;
-      if (key == ConsoleKey.Y) {
-         char[] desArr = result1.OrderDescending ().ToArray ();
-         result = new string (desArr) + result2;
-         Console.WriteLine (string.Join (',', result.Select (c => c)));
-      } else {
-         char[] ascArr = result1.ToArray ();
-         Array.Sort (ascArr);
-         result = new string (ascArr) + result2;
-         Console.WriteLine (string.Join (',', result.Select (c => c)));
-      }
+      var enumerableChar = arr.Where (x => x != spclChar);
+      enumerableChar = (key == ConsoleKey.Y) ? enumerableChar.OrderDescending () : enumerableChar.Order ();
+      var result = enumerableChar.ToList ();
+      result.AddRange (Enumerable.Repeat (spclChar, arr.Length - result.Count));
+      Console.WriteLine (string.Join (',', result));
    }
 
    /// <summary>To check if the given array is not empty and consists of only letters</summary>
    /// <returns>The non-empty char array with letters</returns>
    static char[] CharArray () {
-      while (true) {
+      char[] arr = { };
+      while (arr.Length == 0 || !arr.All (char.IsLetter)) {
          Console.Write ("Enter the character array A: ");
-         char[] arr = Console.ReadLine ().ToCharArray ();
-         if (arr.Length == 0 || !arr.All (char.IsLetter)) continue;
-         else return arr;
+         arr = Console.ReadLine ().ToCharArray ();
       }
+      return arr;
    }
 
-   /// <summary>To check if the given special character is letter</summary>
+   /// <summary>To check if the given special character is a letter</summary>
    /// <returns>The given special character</returns>
    static char SpecialChar () {
       while (true) {
