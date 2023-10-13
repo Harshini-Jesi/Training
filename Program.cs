9fb0477dba2e120af6c0a301fcbe27eede845f2b
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2023-10-13 14:59:44 +0530

    A04-spelling bee extension

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
-}
+﻿Dictionary<char, int> freq = new ();
+var words = File.ReadAllText ("H:/harshini.s/words.txt");
+foreach (var c in words)
+   if (c is >= 'A' and <= 'Z') freq[c] = freq.TryGetValue (c, out int value) ? ++value : 1;
+var sortedList = freq.OrderByDescending (x => x.Value).Take (7);
+foreach (var ele in sortedList) Console.WriteLine (ele);
