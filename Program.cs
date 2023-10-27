2f5f45ca5ce35a0bdb0e955b87e26d737a1a80ee
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2023-10-27 16:06:19 +0530

    added words.txt

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Dictionary<char, int> freq = new ();
-var words = File.ReadAllText ("H:/harshini.s/words.txt");
+var words = File.ReadAllText ("data/words.txt");
 foreach (var c in words)
    if (c is >= 'A' and <= 'Z') freq[c] = freq.TryGetValue (c, out int value) ? ++value : 1;
 var sortedList = freq.OrderByDescending (x => x.Value).Take (7);
