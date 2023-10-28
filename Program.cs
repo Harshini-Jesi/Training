977f4a7262dbb8d3c9ab5a4fe9a63eaa98a1751a
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2023-10-28 10:46:53 +0530

    Minor changes done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿Dictionary<char, int> freq = new ();
 var words = File.ReadAllText ("data/words.txt");
-foreach (var c in words)
-   if (c is >= 'A' and <= 'Z') freq[c] = freq.TryGetValue (c, out int value) ? ++value : 1;
+foreach (var c in words.ToLower())
+   if (char.IsLetter(c)) freq[c] = freq.TryGetValue (c, out int value) ? ++value : 1;
 var sortedList = freq.OrderByDescending (x => x.Value).Take (7);
 foreach (var ele in sortedList) Console.WriteLine (ele);
