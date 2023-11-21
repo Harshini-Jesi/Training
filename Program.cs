d0979f414ed73080f3e730f9213e16d47c230651
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2023-11-21 15:39:57 +0530

    Reviews corrected in T26

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace T26 {
+   #region class Program ---------------------------------------------------------------------
    class Program {
+      #region Methods ---------------------------------------------
       public static void Main () {
          MyList<int> numbers = new ();
          numbers.Add (1);
@@ -13,26 +15,32 @@
          //numbers.RemoveAt (6);
          //numbers.Clear();
          //numbers[8] = 10;
-         for (int i = 0; i < numbers.Capacity; i++)
-            Console.WriteLine (numbers[i]);
+         numbers.Display ();
       }
+      #endregion
    }
+   #endregion
 
+   #region class MyList<T> ---------------------------------------------------------------------
    /// <summary>Class MyList defines the properties and methods of a List of T</summary>
    /// <typeparam name="T">T is type of variables declared in the List</typeparam>
    public class MyList<T> {
 
+      #region Constructor -------------------------------------------
       /// <summary>Construct used to assign the values and count</summary>
       public MyList () {
-         values = new T[4];
-         count = 0;
+         mValues = new T[4];
+         mCount = 0;
       }
+      #endregion
 
-      /// <summary>Gets the no.of elements in the list</summary>
-      public int Count => count;
+      #region Properties --------------------------------------------
 
       /// <summary>Gets the capacity of the list</summary>
-      public int Capacity => values.Length;
+      public int Capacity => mValues.Length;
+
+      /// <summary>Gets the no.of elements in the list</summary>
+      public int Count => mCount;
 
       /// <summary>To get the value in the given index and to set an value to the given index</summary>
       /// <param name="index">The index position to get or set the value</param>
@@ -41,39 +49,33 @@
       public T this[int index] {
          get {
             if (index < 0 || index > Capacity) throw new IndexOutOfRangeException ("Index out of valid range.");
-            return values[index];
+            return mValues[index];
          }
          set {
             if (index < 0 || index > Capacity) throw new IndexOutOfRangeException ("Index out of valid range.");
-            values[index] = value;
+            mValues[index] = value;
          }
       }
+      #endregion
 
+      #region Implementation ----------------------------------------
       /// <summary>Adds an element to the list and resizes its size if required</summary>
       /// <param name="a">The element to be added</param>
       public void Add (T a) {
-         if (count == Capacity) Array.Resize (ref values, count * 2);
-         values[count] = a;
-         count++;
-      }
-
-      /// <summary>Removes an element from the list</summary>
-      /// <param name="a">The element to be removed</param>
-      /// <returns><text>true</text>if the element is removed</returns>
-      /// <exception cref="InvalidOperationException">Throws exception when the element is not found in the list</exception>
-      public bool Remove (T a) {
-         int index = Array.IndexOf (values, a);
-         if (!values.Contains (a)) throw new InvalidOperationException ("Item not found in the list");
-         values[index] = default;
-         for (int i = index; i < Count; i++) values[i] = values[i + 1];
-         count--;
-         return true;
+         if (mCount == Capacity) Array.Resize (ref mValues, mCount * 2);
+         mValues[mCount] = a;
+         mCount++;
       }
 
       /// <summary>Clears all elements from the list</summary>
       public void Clear () {
-         Array.Clear (values, 0, Count);
-         count = 0;
+         Array.Clear (mValues, 0, Count);
+         mCount = 0;
+      }
+
+      /// <summary>To print the elements on stack</summary>
+      public void Display () {
+         for (int i = 0; i < Capacity; i++) Console.WriteLine (mValues[i]);
       }
 
       /// <summary>Inserts an element between two elements in the list</summary>
@@ -82,10 +84,22 @@
       /// <exception cref="IndexOutOfRangeException">Throws exception when the given index below 0 or above the count</exception>
       public void Insert (int index, T a) {
          if (index < 0 || index > Count) throw new IndexOutOfRangeException ("Index out of valid range.");
-         if (count == Capacity) Array.Resize (ref values, count * 2);
-         for (int i = count; i > index; i--) values[i] = values[i - 1];
-         values[index] = a;
-         count++;
+         if (mCount == Capacity) Array.Resize (ref mValues, mCount * 2);
+         for (int i = mCount; i > index; i--) mValues[i] = mValues[i - 1];
+         mValues[index] = a;
+         mCount++;
+      }
+
+      /// <summary>Removes an element from the list</summary>
+      /// <param name="a">The element to be removed</param>
+      /// <returns><text>true</text>if the element is removed</returns>
+      /// <exception cref="InvalidOperationException">Throws exception when the element is not found in the list</exception>
+      public bool Remove (T a) {
+         int index = Array.IndexOf (mValues, a);
+         if (index == -1) throw new InvalidOperationException ("Item not found in the list");
+         for (int i = index; i < Count; i++) mValues[i] = mValues[i + 1];
+         mCount--;
+         return true;
       }
 
       /// <summary>Removes the element in the given index position</summary>
@@ -93,10 +107,13 @@
       /// <exception cref="IndexOutOfRangeException">Throws exception when the given index below 0 (or) greater than or equal to the count</exception>
       public void RemoveAt (int index) {
          if (index < 0 || index >= Count) throw new IndexOutOfRangeException ("Index out of valid range.");
-         var item = values[index];
+         var item = mValues[index];
          Remove (item);
       }
-      T[] values;
-      int count;
+      #endregion
+
+      T[] mValues;
+      int mCount;
    }
+   #endregion
 }
--- TestT26/TestList/UnitTest1.cs
+++ TestT26/TestList/UnitTest1.cs
@@ -1,8 +1,40 @@
+using System;
 using T26;
 namespace TestList {
    [TestClass]
    public class UnitTest1 {
       MyList<int> numbers = new ();
+
+      [TestMethod]
+      public void TestAdd () {
+         numbers.Add (1);
+         numbers.Add (2);
+         numbers.Add (3);
+         Assert.AreEqual (3, numbers.Count);
+         Assert.AreEqual (4, numbers.Capacity);
+         numbers.Add (4);
+         numbers.Add (5);
+         Assert.AreEqual (5, numbers.Count);
+         Assert.AreEqual (8, numbers.Capacity);
+      }
+
+      [TestMethod]
+      public void TestClear () {
+         numbers.Add (1);
+         numbers.Add (2);
+         numbers.Add (3);
+         Assert.AreEqual (3, numbers.Count);
+         numbers.Clear ();
+         Assert.AreEqual (0, numbers.Count);
+      }
+
+      [TestMethod]
+      public void TestDisplay () {
+         numbers.Add (1);
+         numbers.Add (2);
+         numbers.Add (3);
+         numbers.Display ();
+      }
 
       [TestMethod]
       public void TestIndex () {
@@ -19,40 +51,6 @@ namespace TestList {
       }
 
       [TestMethod]
-      public void TestAdd () {
-         numbers.Add (1);
-         numbers.Add (2);
-         numbers.Add (3);
-         Assert.AreEqual (3, numbers.Count);
-         Assert.AreEqual (4, numbers.Capacity);
-         numbers.Add (4);
-         numbers.Add (5);
-         Assert.AreEqual (5, numbers.Count);
-         Assert.AreEqual (8, numbers.Capacity);
-      }
-
-      [TestMethod]
-      public void TestRemove () {
-         numbers.Add (1);
-         numbers.Add (2);
-         numbers.Add (3);
-         numbers.Remove (2);
-         Assert.AreEqual (3, numbers[1]);
-         Assert.AreEqual (2, numbers.Count);
-         Assert.ThrowsException<InvalidOperationException> (() => numbers.Remove (6));
-      }
-
-      [TestMethod]
-      public void TestClear () {
-         numbers.Add (1);
-         numbers.Add (2);
-         numbers.Add (3);
-         Assert.AreEqual (3, numbers.Count);
-         numbers.Clear ();
-         Assert.AreEqual (0, numbers.Count);
-      }
-
-      [TestMethod]
       public void TestInsert () {
          numbers.Add (1);
          numbers.Add (2);
@@ -64,6 +62,17 @@ namespace TestList {
          Assert.AreEqual (8, numbers.Capacity);
          Assert.ThrowsException<IndexOutOfRangeException> (() => numbers.Insert (6, 10));
          Assert.ThrowsException<IndexOutOfRangeException> (() => numbers.Insert (-1, 10));
+      }
+
+      [TestMethod]
+      public void TestRemove () {
+         numbers.Add (1);
+         numbers.Add (2);
+         numbers.Add (3);
+         numbers.Remove (2);
+         Assert.AreEqual (3, numbers[1]);
+         Assert.AreEqual (2, numbers.Count);
+         Assert.ThrowsException<InvalidOperationException> (() => numbers.Remove (6));
       }
 
       [TestMethod]
