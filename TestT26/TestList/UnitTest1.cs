d65fd25ed45fd0a478d51b50ee9bb9b4e651be64
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2023-11-28 13:07:07 +0530

    Modified Testcase

--- TestT26/TestList/UnitTest1.cs
+++ TestT26/TestList/UnitTest1.cs
@@ -5,25 +5,30 @@ namespace TestList {
 
       [TestMethod]
       public void TestAdd () {
-         mNumbers.Add (1);
-         mNumbers.Add (2);
-         mNumbers.Add (3);
-         Assert.AreEqual (3, mNumbers.Count);
-         Assert.AreEqual (4, mNumbers.Capacity);
+         for (int i = 0; i < 4; i++) {
+            mNumbers.Add (i);
+            mList.Add (i);
+         }
+         Assert.AreEqual (mList.Count, mNumbers.Count);
+         Assert.AreEqual (mList.Capacity, mNumbers.Capacity);
          mNumbers.Add (4);
          mNumbers.Add (5);
-         Assert.AreEqual (5, mNumbers.Count);
-         Assert.AreEqual (8, mNumbers.Capacity);
+         mList.Add (4);
+         mList.Add (5);
+         Assert.AreEqual (mList.Count, mNumbers.Count);
+         Assert.AreEqual (mList.Capacity, mNumbers.Capacity);
       }
 
       [TestMethod]
       public void TestClear () {
-         mNumbers.Add (1);
-         mNumbers.Add (2);
-         mNumbers.Add (3);
-         Assert.AreEqual (3, mNumbers.Count);
+         for (int i = 0; i < 4; i++) {
+            mNumbers.Add (i);
+            mList.Add (i);
+         }
+         Assert.AreEqual (mList.Count, mNumbers.Count);
          mNumbers.Clear ();
-         Assert.AreEqual (0, mNumbers.Count);
+         mList.Clear ();
+         Assert.AreEqual (mList.Count, mNumbers.Count);
       }
 
       [TestMethod]
@@ -33,18 +38,20 @@ namespace TestList {
          mNumbers.Add (3);
          mNumbers.Display ();
          Assert.AreEqual (mNumbers[0], 1);
-         Assert.AreEqual (mNumbers[1], 2); 
+         Assert.AreEqual (mNumbers[1], 2);
          Assert.AreEqual (mNumbers[2], 3);
       }
 
       [TestMethod]
       public void TestIndex () {
-         mNumbers.Add (1);
-         mNumbers.Add (2);
-         mNumbers.Add (3);
+         for (int i = 0; i < 4; i++) {
+            mNumbers.Add (i + 1);
+            mList.Add (i + 1);
+         }
          mNumbers[2] = 5;
-         Assert.AreEqual (5, mNumbers[2]);
-         Assert.AreEqual (4, mNumbers.Capacity);
+         mList[2] = 5;
+         Assert.AreEqual (mList[2], mNumbers[2]);
+         Assert.AreEqual (mList.Capacity, mNumbers.Capacity);
          Assert.ThrowsException<IndexOutOfRangeException> (() => mNumbers[5]);
          Assert.ThrowsException<IndexOutOfRangeException> (() => mNumbers[-3]);
          Assert.ThrowsException<IndexOutOfRangeException> (() => mNumbers[7] = 10);
@@ -53,40 +60,46 @@ namespace TestList {
 
       [TestMethod]
       public void TestInsert () {
-         mNumbers.Add (1);
-         mNumbers.Add (2);
-         mNumbers.Add (3);
-         mNumbers.Add (4);
+         for (int i = 0; i < 4; i++) {
+            mNumbers.Add (i + 1);
+            mList.Add (i + 1);
+         }
          mNumbers.Insert (1, 8);
-         Assert.AreEqual (8, mNumbers[1]);
-         Assert.AreEqual (5, mNumbers.Count);
-         Assert.AreEqual (8, mNumbers.Capacity);
+         mList.Insert (1, 8);
+         Assert.AreEqual (mList[1], mNumbers[1]);
+         Assert.AreEqual (mList.Count, mNumbers.Count);
+         Assert.AreEqual (mList.Capacity, mNumbers.Capacity);
          Assert.ThrowsException<IndexOutOfRangeException> (() => mNumbers.Insert (6, 10));
          Assert.ThrowsException<IndexOutOfRangeException> (() => mNumbers.Insert (-1, 10));
       }
 
       [TestMethod]
       public void TestRemove () {
-         mNumbers.Add (1);
-         mNumbers.Add (2);
-         mNumbers.Add (3);
+         for (int i = 1; i < 4; i++) {
+            mNumbers.Add (i);
+            mList.Add (i);
+         }
          mNumbers.Remove (2);
-         Assert.AreEqual (3, mNumbers[1]);
-         Assert.AreEqual (2, mNumbers.Count);
+         mList.Remove (2);
+         Assert.AreEqual (mList[1], mNumbers[1]);
+         Assert.AreEqual (mList.Count, mNumbers.Count);
          Assert.ThrowsException<InvalidOperationException> (() => mNumbers.Remove (6));
       }
 
       [TestMethod]
       public void TestRemoveAt () {
-         mNumbers.Add (1);
-         mNumbers.Add (2);
-         mNumbers.Add (3);
+         for (int i = 1; i < 4; i++) {
+            mNumbers.Add (i);
+            mList.Add (i);
+         }
          Assert.ThrowsException<IndexOutOfRangeException> (() => mNumbers.RemoveAt (4));
          Assert.ThrowsException<IndexOutOfRangeException> (() => mNumbers.RemoveAt (-2));
          mNumbers.RemoveAt (0);
-         Assert.AreEqual (2, mNumbers.Count);
+         mList.RemoveAt (0);
+         Assert.AreEqual (mList.Count, mNumbers.Count);
       }
 
       MyList<int> mNumbers = new ();
+      List<int> mList = new ();
    }
 }
