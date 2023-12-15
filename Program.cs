81af3013c8ea2c4760db8a1fc57c85ee2f330081
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2023-12-15 15:16:19 +0530

    Reviews fixed

--- Program.cs
+++ Program.cs
@@ -21,15 +21,12 @@
          double integral; int exponential;
          // Checks the string that contains 'e'
          if (result.Contains ('e')) {
-            if (SplitLengthCheck (result, 'e', eArray)) {
+            if (SplitLengthCheck (result, 'e')) {
                eArray = result.Split ('e');
                if (IsValid (eArray[0], '.') && IsValid (eArray[1])) {
                   // Conversion of the string before 'e' i.e., integral part
                   if (eArray[0].StartsWith ('.') || eArray[0].StartsWith ('+') || eArray[0].StartsWith ('-')) {
                      integral = StartCheck (eArray[0]);
-                  } else if (eArray[0].Contains ('.')) {
-                     SplitLengthCheck (eArray[0], '.', dotArray);
-                     integral = DoubleConvert (eArray[0]);
                   } else integral = DoubleConvert (eArray[0]);
                   // Conversion of the string after 'e' i.e., exponential part
                   if (eArray[1].StartsWith ('-')) {
@@ -43,45 +40,36 @@
                   return true;
                } else { output = double.NaN; return false; }
             } else { output = double.NaN; return false; }
-         } else if (result.StartsWith ('.') || result.StartsWith ('+') || result.StartsWith ('-')) {
-            output = StartCheck (result);
-            return true;
-         } else if (result.Contains ('.') && IsValid (result, '.') && SplitLengthCheck (result, '.', dotArray)) {
-            output = DoubleConvert (result);
-            return true;
-         } else if (IsValid (result)) {
-            output = DoubleConvert (result);
-            return true;
+         } else if (IsValid (result, '.') || IsValid (result)) {
+            if (result.StartsWith ('.') || result.StartsWith ('+') || result.StartsWith ('-')) {
+               output = StartCheck (result);
+               return true;
+            } else {
+               output = DoubleConvert (result);
+               return true;
+            }
          } else { output = double.NaN; return false; }
       }
 
       /// <summary>To check if the string contains a particular char only once</summary>
       /// <param name="s">The string to be checked</param>
       /// <param name="c">The char to be checked</param>
-      /// <param name="arr">Array that contains the strings splitted w.r.to the char</param>
       /// <returns><text>true</text>if the length of the array is not more than 2</returns>
-      static bool SplitLengthCheck (string s, char c, string[] arr) {
-         arr = s.Split (c);
-         if (arr.Length > 2) return false;
-         return true;
-      }
+      static bool SplitLengthCheck (string s, char c) => s.Split (c).Length <= 2;
 
       /// <summary>To check if the given string is valid to be converted to double</summary>
       /// <param name="s">The string to be checked</param>
       /// <param name="dot">The optional parameter to check if the string with dot is valid</param>
       /// <returns><text>false</text>if the given string is not valid</returns>
       static bool IsValid (string s, char dot = ' ') {
-         string[] arr = { };
-         if (string.IsNullOrEmpty (s)) return false;
-         else if (char.IsDigit (s[0]) || s.StartsWith ('+') || s.StartsWith ('-') || s.StartsWith (dot) || !s.StartsWith ('e')) {
-            if ((s.StartsWith ('+') || s.StartsWith ('-') || s.StartsWith (dot)) && s == s[0].ToString ()) return false;
+         if (string.IsNullOrEmpty (s) || s.StartsWith ('e')) return false;
+         else if ((s.StartsWith ('+') || s.StartsWith ('-') || s.StartsWith (dot)) && s != s[0].ToString ()) {
             string s1 = s.Substring (1);
             if (s1.All (char.IsDigit)) return true;
-            else if (s1.Contains (dot) && SplitLengthCheck (s1, '.', arr)) {
-               string s2 = s1.Replace (".", "");
-               if (s2.All (char.IsDigit)) return true;
-            } else return false;
-         }
+            else if (s.Contains (dot) && DotCheck (s1)) return true;
+            else return false;
+         } else if (s.All (char.IsDigit)) return true;
+         else if (s.Contains (dot) && DotCheck (s)) return true;
          return false;
       }
 
@@ -99,18 +87,14 @@
       /// <returns>The converted double</returns>
       static double DoubleConvert (string s) {
          double numDouble = 0, factorial = 0.1;
-         string[] dotArr = { };
-         if (s.Contains ('.')) {
-            dotArr = s.Split ('.');
-            if (dotArr[0].All (char.IsDigit) && dotArr[1].All (char.IsDigit)) {
-               foreach (char c in dotArr[0]) numDouble = (numDouble * 10) + (c - '0');
-               foreach (char c in dotArr[1]) {
-                  numDouble += (c - '0') * factorial;
-                  factorial *= 0.1;
-               }
-            } else numDouble = double.NaN;
-         } else if (s.All (char.IsDigit)) foreach (char c in s) numDouble = (numDouble * 10) + (c - '0');
-         else numDouble = double.NaN;
+         if (DotCheck (s)) {
+            string[] dotArr = s.Split ('.');
+            foreach (char c in dotArr[0]) numDouble = (numDouble * 10) + (c - '0');
+            foreach (char c in dotArr[1]) {
+               numDouble += (c - '0') * factorial;
+               factorial *= 0.1;
+            }
+         } else foreach (char c in s) numDouble = char.IsDigit (c) ? (numDouble * 10) + (c - '0') : double.NaN;
          return numDouble;
       }
 
@@ -119,23 +103,30 @@
       /// <returns>The converted double</returns>
       static double StartCheck (string s) {
          double intDouble = 0, deci = 0.1;
-         string[] dotArr = { }, eArr = { };
-         if (s.StartsWith ('+')) {
-            string plus = s.Substring (1);
-            if (plus.Contains ('.') && SplitLengthCheck (plus, '.', dotArr)) intDouble = DoubleConvert (plus);
-            else intDouble = DoubleConvert (plus);
-         } else if (s.StartsWith ('-')) {
-            string minus = s.Substring (1);
-            if (minus.Contains ('.') && SplitLengthCheck (minus, '.', dotArr)) intDouble = -1 * DoubleConvert (minus);
-            else intDouble = -1 * DoubleConvert (minus);
-         } else if (s.StartsWith ('.')) {
-            string fact = s.Substring (1);
-            foreach (char c in fact) {
+         string num = s.Substring (1);
+         if (s.StartsWith ('.')) {
+            foreach (char c in num) {
                intDouble += (c - '0') * deci;
                deci *= 0.1;
             }
-         }
+         } else if (DotCheck (num) || num.All (char.IsDigit)) {
+            if (s.StartsWith ('+')) intDouble = DoubleConvert (num);
+            else if (s.StartsWith ('-')) intDouble = -1 * DoubleConvert (num);
+         } else intDouble = double.NaN;
          return intDouble;
+      }
+
+      /// <summary>To check if a given string has only one '.' and digits</summary>
+      /// <param name="s">The string to be checked</param>
+      /// <returns><text>true</text>if the string has only one '.' and digits</returns>
+      static bool DotCheck (string s) {
+         if (s.Contains ('.')) {
+            if (SplitLengthCheck (s, '.')) {
+               string s2 = s.Replace (".", "");
+               if (s2.All (char.IsDigit)) return true;
+            } else return false;
+         } else return false;
+         return false;
       }
    }
 }
