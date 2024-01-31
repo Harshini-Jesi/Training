14e930cc56263b87fb4edabd23c1f70ec94f0753
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2024-01-31 12:20:15 +0530

    P02 completed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   class Program {
+      static void Main () {
+         Console.WriteLine ("Press (C)ircle or (R)ectangle");
+         if (Console.ReadKey (true).Key == ConsoleKey.C) {
+            Circle round = new () {
+               Radius = 5
+            };
+            Console.WriteLine ($"Circle: Radius={round.Radius}, Area={round.Area}");
+         } else if (Console.ReadKey (true).Key == ConsoleKey.R) {
+            Rectangle rect = new () {
+               Length = 5,
+               Width = 5
+            };
+            Console.WriteLine ($"Rectangle: Length={rect.Length}, Width={rect.Width}, Area={rect.Area}");
+         } else Console.WriteLine ("Press C or R");
+      }
+   }
+
+   public class Circle {
+      public double Radius { get; set; }
+
+      public double Area => Math.Round (Math.PI * Radius * Radius, 2);
+   }
+
+   public class Rectangle {
+      public double Length { get; set; }
+
+      public double Width { get; set; }
+
+      public double Area => Length * Width;
+   }
 }
