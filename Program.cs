f014ed84b754c63c820202b591dbfc656862d77c
Harshini-Jesi <harshini.sivabalan@trumpf.com>
2024-03-19 10:47:36 +0530

    A14.1: Anagrams

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   /// <summary>Program to sort and print anagrams</summary>
+   internal class Program {
+      static void Main () {
+         var anagrams = File.ReadAllLines ("C:/etc/words.txt")
+            .GroupBy (s => string.Concat (s.OrderBy (c => c))) // Groups the anagrams
+            .Where (s => s.Count () > 1) // Removes the non anagram words
+            .OrderByDescending (s => s.Count ()); // Sorts in descending order of its count
+         using StreamWriter sw = new ("C:/etc/anagrams.txt");
+         foreach (var word in anagrams) sw.WriteLine ($"{word.Count ()} {string.Join (" ", word)}");
+      }
+   }
 }
